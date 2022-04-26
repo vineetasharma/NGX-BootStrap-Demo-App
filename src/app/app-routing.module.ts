3dd7509aa356b654c29771d5b08233d68f19d9a0
Vineeta Sharma <vineetasharma18791@gmail.com>
2022-04-26 18:31:54 +0530

    Basic Accordion added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
 
-const routes: Routes = [];
+import { AccordionComponent } from './ngx-bootstrap-components-demo/accordion/accordion.component';
+
+const routes: Routes =[
+  {path: 'accordion', component: AccordionComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    AccordionModule.forRoot()],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+    AccordionComponent
+  ]
 })
 export class AppRoutingModule { }

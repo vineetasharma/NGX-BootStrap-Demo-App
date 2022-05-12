5a2064062a4f6d949d71b262f5e14d39933baa43
Vineeta Sharma <vineetasharma18791@gmail.com>
2022-05-12 23:43:19 +0530

    Carousel added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccordionModule} from 'ngx-bootstrap/accordion';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AccordionComponent } from './ngx-bootstrap-components-demo/accordion/accordion.component';
-import { AnimatedAccordionComponent} from './ngx-bootstrap-components-demo/animated-accordion/animated-accordion.component'
+import { AnimatedAccordionComponent} from './ngx-bootstrap-components-demo/animated-accordion/animated-accordion.component';
+import { CarouselComponent } from './ngx-bootstrap-components-demo/carousel/carousel.component'
 
 const routes: Routes =[
   {path: 'accordion', component: AccordionComponent},
-  {path: 'animated-accordion', component: AnimatedAccordionComponent}
+  {path: 'animated-accordion', component: AnimatedAccordionComponent},
+  {path: 'carousel', component: CarouselComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    AccordionModule.forRoot()],
+    AccordionModule.forRoot(),
+    CarouselModule.forRoot()
+  ],
   exports: [
     RouterModule
   ],
   declarations: [
     AccordionComponent,
-    AnimatedAccordionComponent
+    AnimatedAccordionComponent,
+    CarouselComponent
   ]
 })
 export class AppRoutingModule { }

90e30205de25ff2f141a7382312e69928d8d7fcf
Vineeta Sharma <vineetasharma18791@gmail.com>
2022-05-13 23:48:46 +0530

    some css changes applied & added Page Not Found functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AccordionComponent } from './ngx-bootstrap-components-demo/accordion/accordion.component';
 import { AnimatedAccordionComponent} from './ngx-bootstrap-components-demo/animated-accordion/animated-accordion.component';
-import { CarouselComponent } from './ngx-bootstrap-components-demo/carousel/carousel.component'
+import { CarouselComponent } from './ngx-bootstrap-components-demo/carousel/carousel.component';
+import { PageNotFoundComponent } from './ngx-bootstrap-components-demo/page-not-found/page-not-found.component';
 
 const routes: Routes =[
   {path: 'accordion', component: AccordionComponent},
   {path: 'animated-accordion', component: AnimatedAccordionComponent},
-  {path: 'carousel', component: CarouselComponent}
+  {path: 'carousel', component: CarouselComponent},
+  {path: '', component: AccordionComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({

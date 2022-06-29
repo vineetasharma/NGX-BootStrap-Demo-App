020aa40616152dad55717799397eba6698a0c624
Vineeta Sharma <vineetasharma18791@gmail.com>
2022-06-29 17:08:06 +0530

    subject added

--- src/app/ngx-bootstrap-components-demo/page-not-found/page-not-found.component.ts
+++ src/app/ngx-bootstrap-components-demo/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnChanges, DoCheck,OnDestroy,AfterViewInit,AfterContentChecked,AfterContentInit,AfterViewChecked } from '@angular/core';
+import { Observable, Observer} from 'rxjs'
+
 
 @Component({
   selector: 'app-page-not-found',
@@ -8,11 +10,41 @@ import { Component, OnInit, OnChanges, DoCheck,OnDestroy,AfterViewInit,AfterCont
 export class PageNotFoundComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   title ='PAGE NOT FOUND';
+  greeting: Promise<string>|null = null;
+  arrived: boolean = false;
+
+  private resolve: Function|null = null;
+
   constructor() {
     console.log('Constructor called!!');
+    this.reset();
    }
 
+   time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+  
+   reset() {
+    this.arrived = false;
+    this.greeting = new Promise<string>((resolve, reject) => {
+      this.resolve = resolve;
+    });
+  }
+
+  clicked() {
+    if (this.arrived) {
+      this.reset();
+    } else {
+      this.resolve!('hi there!');
+      this.arrived = true;
+    }
+  }
+
   ngOnInit(): void {
+    let counter= 0;
+    setTimeout(()=>{
+      this.resolve!(++counter);
+    },1500);
     console.log('ngOnInit called!!');
   }
 
@@ -21,9 +53,7 @@ export class PageNotFoundComponent implements OnInit, OnChanges, AfterContentIni
   }
 
   ngAfterContentInit(): void {
-
     console.log('ngAfterContentInit called!!');
-
   }
 
   ngAfterContentChecked(): void {
--- src/app/ngx-bootstrap-components-demo/rx-js-features/rx-js-features.component.ts
+++ src/app/ngx-bootstrap-components-demo/rx-js-features/rx-js-features.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ajax } from 'rxjs/ajax';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-rx-js-features',
@@ -12,6 +12,7 @@ export class RxJsFeaturesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.subjectExample();
 
     forkJoin(
       // as of RxJS 6.5+ we can use a dictionary of sources
@@ -23,6 +24,32 @@ export class RxJsFeaturesComponent implements OnInit {
     )
       // { google: object, microsoft: object, users: array }
       .subscribe(console.log);
+  }
+
+
+  subjectExample(){
+    let subject= new Subject();
+    subject.subscribe({
+      next: (value) => console.log('Subscriber A called:::---',value),
+    });
+    subject.subscribe({
+      next: (value) => console.log('Subscriber B called:::---',value),
+    });
+    subject.subscribe({
+      next: (value) => console.log(),
+    });
+
+    console.log('subject----',subject);
+
+    subject.next(1);
+    subject.next(2);
+    subject.next('VINI');
+    subject.subscribe({
+      next: (value) => console.log('Subscriber D called:::---',value),
+    });
+
+    subject.next('Sharma');
+
   }
 
 }
